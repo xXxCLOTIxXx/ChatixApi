3ab2c3bbda76bc3b0acf849193b387cc441ea5df
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2022-06-04 02:11:42 +0300

    Update send-image1.py

--- examples/send-image1.py
+++ examples/send-image1.py
@@ -4,8 +4,4 @@ name = 'Bot name'
 fileUrl = 'link'
 
 client = Client(name=name)
-def on_msg():
-	client.send_message(fileUrl=fileUrl)
-
-if __name__ == '__main__':
-	on_msg()
+client.send_message(fileUrl=fileUrl)
